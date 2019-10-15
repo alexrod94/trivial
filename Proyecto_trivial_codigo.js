b7ae032b51c9bf8a1e543cd2afe6b22c7aa0dd71
Alex Rodríguez <alexrod94@gmail.com>
2019-10-15 18:57:15 +0200

    asfas

---
+++ Proyecto_trivial_codigo.js
@@ -0,0 +1,75 @@
+let jugadores = [
+    {
+        nombre: 1,
+        ciencia: false,
+        historia: false,
+        literatura: false,
+        ocio: false
+    }, {
+        nombre: 2,
+        ciencia: false,
+        historia: false,
+        literatura: false,
+        ocio: false
+    }
+]
+
+let ciencia = [{
+id: 0,
+pregunta: "",
+respuesta: ""
+}, {
+    id: 1,
+    pregunta: "",
+    respuesta: ""
+    }, {
+    id: 2,
+    pregunta: "",
+    respuesta: ""
+    }
+]
+
+let historia = [{
+    id: 0,
+    pregunta: "",
+    respuesta: ""
+    }, {
+        id: 1,
+        pregunta: "",
+        respuesta: ""
+        }, {
+        id: 2,
+        pregunta: "",
+        respuesta: ""
+        }
+    ]
+
+let literatura = [{
+    id: 0,
+    pregunta: "",
+    respuesta: ""
+    }, {
+        id: 1,
+        pregunta: "",
+        respuesta: ""
+        }, {
+        id: 2,
+        pregunta: "",
+        respuesta: ""
+        }
+    ]
+
+let ocio = [{
+    id: 0,
+    pregunta: "",
+    respuesta: ""
+    }, {
+        id: 1,
+        pregunta: "",
+        respuesta: ""
+        }, {
+        id: 2,
+        pregunta: "",
+        respuesta: ""
+        }
+    ]
